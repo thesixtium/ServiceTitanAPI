5f710211ee18bcb266021fd513cfb6b07ca553e8
aberezowski <ajrberezowski@gmail.com>
2022-12-18 23:03:43 -0700

    Finished "get_modified_jobs" method

--- integration.py
+++ integration.py
@@ -1,6 +1,7 @@
 import ServiceTitan
 import password
 
+
 class Integration:
     def servicetitan_jobs_to_close_opportunities(self, jobs):
         modified_jobs = []
@@ -18,14 +19,29 @@ class Integration:
 
         return modified_jobs
 
-    def run(self):
+    def test_modified_jobs(self):
         service_titan_bot = ServiceTitan.ServiceTitan(
-            password.get_close_api_key(),
             password.get_client_id(),
             password.get_client_secret(),
             password.get_app_key(),
             password.get_tenant_id()
         )
 
-        jobs = service_titan_bot.get_modified_jobs()
+        f = open("test_modified_jobs.txt", "w")
+
+        jobs = service_titan_bot.get_modified_jobs(limit_amount=6)
         modified_jobs = self.servicetitan_jobs_to_close_opportunities(jobs)
+
+        f.write("Jobs:\n")
+        for job in jobs:
+            f.write(f"{job}\n")
+
+        f.write("\n\nModified Jobs:\n")
+        for job in modified_jobs:
+            f.write(f"{job}\n")
+
+        f.close()
+
+if __name__ == '__main__':
+    integration = Integration()
+    integration.test_modified_jobs()
--- servicetitan.py
+++ servicetitan.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 import json
 import requests
 
@@ -18,10 +19,12 @@ class ServiceTitan:
             self.environment_url = "https://api.servicetitan.io/"
         else:
             self.environment_url = "https://api-integration.servicetitan.io/"
-        self.client_secret = client_id
+
+        self.client_id = client_id
         self.client_secret = client_secret
         self.app_key = app_key
         self.tenant_id = tenant_id
+
         self.debug_mode = debug_mode
         self.access_token = self.get_access_token()
 
@@ -36,32 +39,69 @@ class ServiceTitan:
                + '&client_secret=' \
                + self.client_secret
 
-        response = requests.post('https://auth.servicetitan.io/connect/token', headers=headers, data=data)
+        if self.environment_url == "https://api.servicetitan.io/":
+            response = requests.post('https://auth.servicetitan.io/connect/token', headers=headers, data=data)
+        else:
+            response = requests.post('https://auth-integration.servicetitan.io/connect/token', headers=headers,
+                                     data=data)
+
         res = json.loads(response.content.decode())
 
         return res["access_token"]
 
     # Get all jobs modified within the last _ days
-    def get_modified_jobs(self, number_of_days=1):
-        request_url = self.environment_url \
+    def get_modified_jobs(self, number_of_days=200, limit_amount=0):
+        # 1 index the amount instead of 0 index
+        limit_amount -= 1
+
+        # Get the day needed for modifiedOnOrAfter
+        today = datetime.now()
+        delta = timedelta(days=number_of_days)
+        modified_on_or_after = today - delta
+
+        # Initialize needed variables
+        page_number = 1
+        has_more = True
+        return_jobs = []
+
+        # While there are more results to go over
+        while has_more:
+
+            # Get response
+            request_url = self.environment_url \
                       + 'jpm/v2/tenant/' \
                       + self.tenant_id \
-                      + '/jobs'
+                      + f'/jobs?page={page_number}' \
+                        f'&pageSize=8' \
+                        f'&modifiedOnOrAfter={modified_on_or_after.isoformat("T")}'
 
-        headers = {
-            'Authorization': self.get_access_token(),
-            'ST-App-Key': self.app_key
-        }
+            headers = {
+                'Authorization': self.access_token,
+                'ST-App-Key': self.app_key
+            }
 
-        response = requests.get(request_url, headers=headers)
-        response = response.content.decode()
-        res = json.loads(response)
+            response = requests.get(request_url, headers=headers)
+            response = response.content.decode()
+            res = json.loads(response)
 
-        return_jobs = []
-        for job in res["data"]:
-            modified_on = datetime.strptime(job["modifiedOn"], "%m/%d/%y %H:%M:%S")
-            if (modified_on - datetime.now()).days > number_of_days:
+            # For every job in the response
+            for job in res["data"]:
+                # Append to return jobs
                 return_jobs.append(self.get_job_invoice_info(job))
+
+                # If limiting amount of jobs, break if over the limit
+                if len(return_jobs) > limit_amount > 0:
+                    break
+
+            # Increase page number
+            page_number += 1
+
+            # Update hasMore variable
+            has_more = res["hasMore"]
+
+            # If limiting amount of jobs, break if over the limit
+            if len(return_jobs) > limit_amount > 0:
+                break
 
         return return_jobs
 
@@ -74,7 +114,7 @@ class ServiceTitan:
                       + str(job["id"])
 
         headers = {
-            'Authorization': self.get_access_token(),
+            'Authorization': self.access_token,
             'ST-App-Key': self.app_key
         }
 
@@ -101,7 +141,7 @@ class ServiceTitan:
                       + str(job["locationId"])
 
         headers = {
-            'Authorization': self.get_access_token(),
+            'Authorization': self.access_token,
             'ST-App-Key': self.app_key
         }
 
